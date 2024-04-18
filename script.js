42337dec265e24055e08b444124754353ac269be
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-04-17 20:02:49 -0400

    changed layout for gif

--- script.js
+++ script.js
@@ -167,26 +167,26 @@ shipsBtnEl.addEventListener("click", function () {
       }
     });
 });
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+const btn = document.getElementById("myBtn");
+
+
+const span = document.getElementsByClassName("close")[0];
+
+
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
